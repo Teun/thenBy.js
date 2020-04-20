40b8e7ba83e320865ed660540bf5c8c40c75f1d3
Teun Duynstee <teun@duynstee.com>
2020-04-20 17:35:22 +0200

    #34 fix gulp + vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var insert = require('gulp-insert');
-var replace = require('gulp-replace');
-var umd = require('gulp-umd');
-var mochaPhantomjs = require('gulp-mocha-phantomjs');
-var mocha = require('gulp-mocha');
+const {dest, src, series, parallel} = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const insert = require('gulp-insert');
+const replace = require('gulp-replace');
+const umd = require('gulp-umd');
+const mocha = require('gulp-mocha');
 
-gulp.task('minify', function() {
-    return gulp.src('thenBy.js')
+function minify() {
+    return src('thenBy.js')
         .pipe(umd({
             exports: function (file) {
                 return 'firstBy';
@@ -19,29 +18,26 @@ gulp.task('minify', function() {
         }))
         .pipe(uglify())
         .pipe(rename({suffix:'.min'}))
-        .pipe(insert.prepend('/*** Copyright 2013 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/\n'))
-        .pipe(gulp.dest('.'));
-});
-
-gulp.task('module', function() {
-    return gulp.src('thenBy.js')
+        .pipe(insert.prepend('/*** Copyright 2020 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/\n'))
+        .pipe(dest('.'));
+}
+function module() {
+    return src('thenBy.js')
         .pipe(replace(/var firstBy = \(/, 'module.exports = ('))
         .pipe(rename({suffix:'.module'}))
-        .pipe(gulp.dest('.'));
-});
+        .pipe(dest('.'));
+}
+const build = parallel(minify, module);
 
-gulp.task('build', ['minify', 'module']);
-
-gulp.task('mochaphantom', ['build'], () => {
-    return gulp.src('tests/index.html')
-        .pipe(mochaPhantomjs({reporter: 'tap'}));
-});
-
-gulp.task('mochanode', ['build'], () => {
-    return gulp.src('tests/thenby.tests.js', {read: false})
-        .pipe(mocha({reporter: 'tap', ui :'tdd'}))
-});
-
-gulp.task('test', [/*'mochaphantom',*/ 'mochanode']);
-
-gulp.task('default', ['test']);
+const mochanode = series(
+    build, 
+    () => {
+        return src('tests/thenby.tests.js', {read: false})
+            .pipe(mocha({reporter: 'tap', ui :'tdd'}));
+    });
+exports.minify = minify;
+exports.module = module;
+exports.build = build;
+exports.mochanode = mochanode;
+exports.test = mochanode;
+exports.default = exports.test;
