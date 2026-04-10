18006e25afb764ff28bcec77cd81d04ba52fe731
Teun Duynstee <github@duynstee.com>
2026-04-10 15:06:47 +0200

    Add ESM build, exports map, fix TS declarations, bump to 1.4.0 (#59)
    - Add thenBy.esm.js output (export { firstBy } + export default)
    - Migrate build script from build.js (CJS) to build.mjs (ESM)
    - Add package.json exports map with import/require/types conditions
    - Add module field, sideEffects: false
    - Fix thenBy.module.d.ts: class -> interface, fix shadowed type params,
      add top-level exports for node16/bundler moduleResolution
    - Update copyright year to 2026 throughout
    - Update inlined script in sample.htm to current thenBy.min.js
    - Version bump 1.3.5 -> 1.4.0
    
    Co-authored-by: Teun Duynstee <teun@duynstee.com>

--- thenBy.module.d.ts
+++ thenBy.module.d.ts
@@ -1,51 +1,60 @@
 // Type definitions for thenBy
 // Definitions by: Teun Duynstee (with significant help from @HonoluluHenk)
 type SortOrder = "asc" | "desc" | -1 | 1;
-declare class opt {
-    direction?:SortOrder;
-    ignoreCase?:boolean;
+
+interface Opt {
+    direction?: SortOrder;
+    ignoreCase?: boolean;
 }
-declare class typedOpt<T> extends opt {
-    cmp?: (a:T, b:T)=> number;
+
+interface TypedOpt<T> extends Opt {
+    cmp?: (a: T, b: T) => number;
 }
+
 interface IThenBy<T> {
-    (v1: T, v2: T) : number;
-    /** 
+    (v1: T, v2: T): number;
+    /**
      * Full format to compare two elements and determine which sorts first.
      * @param compare function that receives two values from the sorted array and returns a number indicating which comes first: < 0: first comes first, 0: doesn't matter, > 0: second comes first.
      * @param direction can be used to reverse the sorting by passing -1
-    **/
-   thenBy<T>(compare: ((v1: T, v2: T) => number), direction?: SortOrder | opt): IThenBy<T>;
-    /** 
+     **/
+    thenBy(compare: (v1: T, v2: T) => number, direction?: SortOrder | Opt): IThenBy<T>;
+    /**
      * Shorthand for selecting a value to sort on from the sorted element.
      * @param select function that receives a value from the sorted array and selects the thing to sort on
      * @param direction reverse by passing -1. opt for other options
-    **/
-   thenBy<T, U>(select: ((v: T) => U), direction?: SortOrder | typedOpt<U>): IThenBy<T>;
-    /** 
+     **/
+    thenBy<U>(select: (v: T) => U, direction?: SortOrder | TypedOpt<U>): IThenBy<T>;
+    /**
      * Shorthand for sorting on a simple property.
      * @param byPropertyName is the name of the property to sort on as a string
      * @param direction reverse by passing -1. opt for other options
-    **/
-    thenBy<T>(byPropertyName: (keyof T), direction?: SortOrder | typedOpt<any>): IThenBy<T>;
+     **/
+    thenBy(byPropertyName: keyof T, direction?: SortOrder | TypedOpt<any>): IThenBy<T>;
 }
+
+/**
+ * Full format to compare two elements and determine which sorts first.
+ * @param compare function that receives two values from the sorted array and returns a number indicating which comes first: < 0: first comes first, 0: doesn't matter, > 0: second comes first.
+ * @param direction can be used to reverse the sorting by passing -1
+ **/
+export function firstBy<T>(compare: (v1: T, v2: T) => number, direction?: SortOrder | Opt): IThenBy<T>;
+/**
+ * Shorthand for selecting a value to sort on from the sorted element.
+ * @param select function that receives a value from the sorted array and selects the thing to sort on
+ * @param direction reverse by passing -1. opt for other options
+ **/
+export function firstBy<T, U>(select: (v: T) => U, direction?: SortOrder | TypedOpt<U>): IThenBy<T>;
+/**
+ * Shorthand for sorting on a simple property.
+ * @param byPropertyName is the name of the property to sort on as a string
+ * @param direction reverse by passing -1. opt for other options
+ **/
+export function firstBy<T>(byPropertyName: keyof T, direction?: SortOrder | TypedOpt<any>): IThenBy<T>;
+
+export default firstBy;
+
+// Ambient module declaration for legacy moduleResolution modes
 declare module "thenby" {
-    /** 
-     * Full format to compare two elements and determine which sorts first.
-     * @param compare function that receives two values from the sorted array and returns a number indicating which comes first: < 0: first comes first, 0: doesn't matter, > 0: second comes first.
-     * @param direction can be used to reverse the sorting by passing -1
-    **/
-    export function firstBy<T>(compare: ((v1: T, v2: T) => number), direction?: SortOrder | opt): IThenBy<T>;
-    /** 
-     * Shorthand for selecting a value to sort on from the sorted element.
-     * @param select function that receives a value from the sorted array and selects the thing to sort on
-     * @param direction reverse by passing -1. opt for other options
-    **/
-    export function firstBy<T,U>(select: ((v: T) => U), direction?: SortOrder | typedOpt<U>): IThenBy<T>;
-    /** 
-     * Shorthand for sorting on a simple property.
-     * @param byPropertyName is the name of the property to sort on as a string
-     * @param direction reverse by passing -1. opt for other options
-    **/
-    export function firstBy<T>(byPropertyName: (keyof T), direction?: SortOrder | typedOpt<any>): IThenBy<T>;
-  }
+    export { firstBy, IThenBy, Opt, TypedOpt, SortOrder };
+}
